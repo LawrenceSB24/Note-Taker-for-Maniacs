ea99d3d4b64c8fface7b445cb55bdf95b57b4965
LawrenceSB24 <lawrs2022@gmail.com>
2022-03-11 19:38:34 -0500

    File to run note taker back end

---
+++ server.js
@@ -0,0 +1,38 @@
+// Imports express
+const express = require('express');
+
+// Imports path
+const path = require('path');
+
+// Imports api routes
+const api = require('./routes/taker.js');
+
+// Constant for local host port
+const PORT = process.env.PORT || 3001;
+
+// Initializtion of app variable
+const app = express();
+
+// Imports cutom middleware, "clog"
+app.use(clog);
+
+// Middleware for parsin JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET route for homepage
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+// GET route for notes page
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/pages/notes.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`App is listening at http://localhost:${PORT} 🚀`);
+});
