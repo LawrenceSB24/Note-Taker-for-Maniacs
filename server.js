c81059136a50b97ef0d85c65278aca45049528ef
LawrenceSB24 <lawrs2022@gmail.com>
2022-03-11 20:07:50 -0500

    Something isnt computing

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get('/', (req, res) => {
 
 // GET route for notes page
 app.get('/notes', (req,res) => {
-    res.sendFile(path.join(__dirname, './public/pages/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 app.listen(PORT, () => {
