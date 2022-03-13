9217de8f70e06fca4c05dc10a0afdc6be55560af
LawrenceSB24 <lawrs2022@gmail.com>
2022-03-12 20:17:41 -0500

    Added some functionality

--- server.js
+++ server.js
@@ -24,13 +24,13 @@ app.use('/api', api);
 app.use(express.static("public"));
 
 // GET route for homepage
-app.get("/", (req, res) => 
+app.get("*", (req, res) => 
     res.sendFile(path.join(__dirname, "./public/index.html"))
 );
 
 // GET route for notes page
 app.get("/api/notes", (req,res) => 
-    res.sendFile(path.join(__dirname, "/public/notes.html"))
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
 );
 
 app.listen(PORT, () => 
