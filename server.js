daf523e8243e982c55cbea6c693be260142f65cb
LawrenceSB24 <lawrs2022@gmail.com>
2022-03-12 20:41:41 -0500

    Adding...

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({extended : true}));
 app.use(express.static("public"));
 
 // GET route for homepage
-app.get('*', (req, res) => 
+app.get('/', (req, res) => 
     res.sendFile(path.join(__dirname, "./public/index.html"))
 );
 
